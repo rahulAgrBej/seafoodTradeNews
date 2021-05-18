bbf8b70cf3e8cbea985b2ee7f9cf704753061593
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-05-17 20:00:16 -0400

    includes newsShifts

--- predictiveModeling.R
+++ predictiveModeling.R
@@ -3,15 +3,19 @@ library(tidyverse)
 library(ISLR)
 library(caret)
 
-dataPath = 'data/original/tradeNews.csv'
+dataPath = 'data/original/tradeNewsShifts.csv'
 df <- read_csv(dataPath)
 
 indexTrainTest <- caret::createDataPartition(df$TRADE_SHOCK, p=0.8, times=1, list=FALSE)
 trainSet <- df[indexTrainTest,]
 testSet <- df[-indexTrainTest,]
 
-fit <- glm(TRADE_SHOCK ~ NEWS_NO_SHIFT, data=trainSet, family=binomial)
-testing <- predict(fit, newdata=testSet, type = "response")
-testing > 0.1
-#summary(fit)
-summary(testing)
+fit.noshift <- glm(TRADE_SHOCK ~ NEWS_NO_SHIFT, data=trainSet, family=binomial)
+fit.shift_back_1 <- glm(TRADE_SHOCK ~ NEWS_BACK_1, data=trainSet, family=binomial)
+fit.shift_back_2 <- glm(TRADE_SHOCK ~ NEWS_BACK_2, data=trainSet, family=binomial)
+fit.shift_back_3 <- glm(TRADE_SHOCK ~ NEWS_BACK_3, data=trainSet, family=binomial)
+fit.shift_forward_1 <- glm(TRADE_SHOCK ~ NEWS_FORWARD_1, data=trainSet, family=binomial)
+fit.shift_forward_2 <- glm(TRADE_SHOCK ~ NEWS_FORWARD_2, data=trainSet, family=binomial)
+fit.shift_forward_3 <- glm(TRADE_SHOCK ~ NEWS_FORWARD_3, data=trainSet, family=binomial)
+
+print(summary(fit.noshift))
