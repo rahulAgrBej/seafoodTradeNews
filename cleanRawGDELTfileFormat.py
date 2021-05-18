5de327d1ef3c59d73da08c64c6608d7ec5f9a69d
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-05-17 21:46:57 -0400

    file that cleans the raw GDELT return files

---
+++ cleanRawGDELTfileFormat.py
@@ -0,0 +1,40 @@
+
+import os
+import pandas as pd
+
+folderPath = 'data/news/raw/original/GDELT_results_unaltered'
+fileNames = os.listdir(folderPath)
+
+
+outLines = ''
+
+for fileName in fileNames:
+    fullPath = os.path.join(folderPath, fileName)
+    print(fileName)
+    if fileName[-4:] != '.csv':
+        print(f'here {fileName} {fileName[-3:]}')
+        continue
+    f = open(fullPath, 'r')
+    lines = f.readlines()
+    header = lines[0]
+
+    lines = lines[1:]
+    for line in lines:
+        line = line.rstrip('\n')
+        cells = line.split(',')
+        
+        if len(cells) == 12:
+            line = line + '\n'
+        else:
+            line = ','.join(cells[:8]) + ',MALFORMED.com,' + ','.join(cells[-3:]) + '\n'
+        
+        outLines = outLines + line
+        #print(line)
+    
+    outFolderPath = 'data/news/processed/original/GDELT_results_cleaned'
+    outFileName = fileName
+    outFilePath = os.path.join(outFolderPath, outFileName)
+    outF = open(outFilePath, 'w')
+    outF.write(header)
+    outF.write(outLines)
+    outF.close()
